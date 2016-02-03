f557cb09eea78c6d46f510568f9b16a6914e8bdc
Nils Schwabe <nils@schwabe.ws>
2016-02-03 17:46:36 +0100

    Make command line arguments more generic

--- serialplot.py
+++ serialplot.py
@@ -13,20 +13,24 @@ import time
 
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
-magPlot = pg.plot()
-magPlot.setWindowTitle('live plot from ble')
-magPlot.addLegend()
+plt = pg.plot(title="SerialPlotPy")
+plt.addLegend()
 
-data_x  = []
-data_y  = []
-data_z  = []
-data_ir = []
+pause = False
 
-freq = 0.0
-freq_zc = 0.0
+data = []
+plots = []
+colors = []
+colors.append(QtGui.QColor(255,0,0));
+colors.append(QtGui.QColor(0,200,0));
+colors.append(QtGui.QColor(0,0,255));
+colors.append(QtGui.QColor(255,0,255));
+colors.append(QtGui.QColor(0,0,0));
+colors.append(QtGui.QColor(103,48,175));
+colors.append(QtGui.QColor(239,239,57));
 
 def signal_handler(signal, frame):
-        print ('Good bye!')
+        print ("\nQuitting...")
         app.closeAllWindows()
         sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
@@ -48,11 +52,10 @@ def check_baud(value):
 
 def listen(port, baud, range, display):
     _sum = 0
-    # start serial connection
-    print("[SerialListener] Connecting to ", args.port, " at ", args.baud, " Bps ...")
+    print("[SerialPlotPy] Connecting to " + str(args.port) + " at " + str(args.baud) + " Bps ...")
     ser = serial.Serial(port, baud)
-    print("[SerialListener] Connected")
-    i = 0
+    print("[SerialPlotPy] Connected")
+    m = 0
     n = 0
     while True:
         try:
@@ -62,75 +65,67 @@ def listen(port, baud, range, display):
             print("No serial connection. Wrong baud rate?")
             app.closeAllWindows()
             sys.exit(1)
-        #print(val)
         if "#" in val:
             # used for comments
             continue
         d = val.split(",")
-        if "x" in display:
+        i = 0
+        while i < len(data):
             try:
-                data_x.append(float(d[0]))
+                data[i].append(float(d[i]))
             except:
-                data_x.append(0);
-        if "y" in display:
-            try:
-                data_y.append(float(d[1]))
-            except:
-                data_y.append(0)
-        if "z" in display:
-            try:
-                data_z.append(float(d[2]))
-            except:
-                data_z.append(0)
-        if "i" in display:
-            try:
-                data_ir.append((float(d[3]) - 40000)/20)
-            except:
-                data_ir.append(0)
-        i = i + 1
+                data[i].append(0.0)
+            i = i + 1
+        m = m + 1
         n = n + 1
-        if i > 10:
-            if "x" in display:
-                x.setData(data_x)
-            if "y" in display:
-                y.setData(data_y)
-            if "z" in display:
-                z.setData(data_z)
-            if "i" in display:
-                ir.setData(data_ir)
+        if m > 10:
             i = 0
-        magPlot.setXRange(n-range, n)
+            while i < len(data):
+                plots[i].setData(data[i])
+                i = i + 1
+            m = 0
+        plt.setXRange(n-range, n)
 
 def check_connection():
-    if (len(data_x) == 0 and len(data_y) == 0 and len(data_z) == 0 and len(data_ir) == 0):
-        print("There is no incoming data. Wrong baud rate?")
-        app.closeAllWindows()
-        sys.exit(1)
+    for i in range(len(data)):
+        if len(data[i]) != 0:
+            return
+    print("There is no incoming data. Wrong baud rate?")
+    app.closeAllWindows()
+    sys.exit(1)
 
 def exitHandler():
-    print("X pressed")
-    quit = True
+    print("Quitting...")
     sys.exit(0)
 
-parser = argparse.ArgumentParser(description="Plot live magnetic data read from serial.")
-parser.add_argument("-p", "--port", help="Serial port", default="/dev/ttyUSB0", type=check_port)
-parser.add_argument("-b", "--baud", help="Baud rate", default="115200", type=check_baud)
-parser.add_argument("-d", "--display", help="Values to display (x,y,z,i)", default="x")
-parser.add_argument("-r", "--range", help="What range to use", default="200", type=int)
+parser = argparse.ArgumentParser(description="Plot live data from serial. Default: /dev/ttyUSB0 with 115200.")
+parser.add_argument("-p", "--port", help="serial port", default="/dev/ttyUSB0", type=check_port)
+parser.add_argument("-b", "--baud", help="baud rate", default="115200", type=check_baud)
+parser.add_argument("-d", "--display", help="displayed values", default=1, type=int)
+parser.add_argument("-r", "--range", help="displayed range", default="200", type=int)
+parser.add_argument("-f", "--file", help="save values to csv file FILE", default="", type=str)
+parser.add_argument("-l", "--labels", help="labels of values in DISPLAY separated by comma", default="-1", type=str)
 args = parser.parse_args()
 
-if "x" in args.display:
-    x = magPlot.plot(pen=pg.mkPen(color=QtGui.QColor(255,0,0), style=QtCore.Qt.SolidLine), name='Magnetic x')
-if "y" in args.display:
-    y = magPlot.plot(pen=pg.mkPen(color=QtGui.QColor(0,255,0), style=QtCore.Qt.SolidLine), name='Magnetic y')
-if "z" in args.display:
-    z = magPlot.plot(pen=pg.mkPen(color=QtGui.QColor(0,255,255), style=QtCore.Qt.SolidLine), name='Magnetic z')
-if "i" in args.display:
-    ir = magPlot.plot(pen=pg.mkPen(color=QtGui.QColor(0,0,255), style=QtCore.Qt.SolidLine), name='Infra-Red')
+# parse labels
+if args.labels == "-1":
+    labels = []
+    for i in range(args.display):
+        labels.append(str(i+1))
+else:
+    labels = args.labels.replace(" ", "").split(",")
+if len(labels) != args.display:
+    print("Amount of labels needs to be the same as DISPLAY!")
+    sys.exit(1)
+
+for i in range(args.display):
+    d = []
+    data.append(d)
+    p = plt.plot(pen=pg.mkPen(color=colors[i % (len(colors))], style=QtCore.Qt.SolidLine), name=labels[i])
+    plots.append(p)
 
 serial_thread = Thread(target=listen, args=(args.port, args.baud, args.range, args.display,))
 serial_thread.daemon = True
-debug("Starting serial thread")
 serial_thread.start()
 
 # after 2 seconds the connection is checked
